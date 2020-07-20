506acab4e9f8d355237d81f87fa948a9ab337904
Tim Leverentz <timlev@gmail.com>
2020-07-20 16:37:38 -0500

    Fixed findmatchingwords.py to count needed words. Updated matching_sound_files_needed.txt

--- pythonscripts/findmatchingwords.py
+++ pythonscripts/findmatchingwords.py
@@ -1,13 +1,13 @@
 import os, glob
 from bs4 import BeautifulSoup
 
-matchingfiles = glob.glob("*//PPTX/*.html")
+matchingfiles = glob.glob("../*//PPTX/*.html")
 
 ##print(matchingfiles)
 
 soundfilesrequired = []
 wordsrequired = []
-soundfilesdownloaded = [os.path.basename(x) for x in glob.glob("units/sounds/*")]
+soundfilesdownloaded = [os.path.basename(x) for x in glob.glob("../units/sounds/*")]
 soundfilesneeded = []
 
 for filename in matchingfiles:
@@ -26,5 +26,5 @@ soundfilesrequired = list(set(soundfilesrequired))
 #print(len(soundfilesdownloaded),len(soundfilesrequired))
 soundfilesneeded = [x for x in soundfilesrequired if x not in soundfilesdownloaded]
 #print([x.replace(".mp3","") for x in soundfilesneeded], len(soundfilesneeded))
-for x in soundfilesdownloaded:
+for x in soundfilesneeded:
 	print(x.replace(".mp3",""))
