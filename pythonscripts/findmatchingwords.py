8dcc74c6b1aa8b61b338ec962b658685aa31cdba
Tim Leverentz <timlev@gmail.com>
2020-07-20 18:16:28 -0500

    Moved sound files around

--- pythonscripts/findmatchingwords.py
+++ pythonscripts/findmatchingwords.py
@@ -10,6 +10,13 @@ wordsrequired = []
 soundfilesdownloaded = [os.path.basename(x) for x in glob.glob("../units/sounds/*")]
 soundfilesneeded = []
 
+learning_english_sound_files = [os.path.basename(x) for x in os.listdir("/home/levtim/GitProjects/Learning-English-HTML/sounds/") if x.endswith(".mp3")]
+read_story_sound_files = [os.path.basename(x) for x in os.listdir("/home/levtim/GitProjects/read-story/sounds/") if x.endswith(".mp3")]
+combined_sound_files = soundfilesdownloaded + learning_english_sound_files + read_story_sound_files
+
+#print(combined_sound_files)
+#print(len(combined_sound_files))
+
 for filename in matchingfiles:
 	soup = BeautifulSoup(open(filename), "html5lib")
 	soundtags = soup.find_all("audio")
@@ -24,7 +31,8 @@ wordsrequired = list(set(wordsrequired))
 soundfilesrequired = list(set(soundfilesrequired))
 
 #print(len(soundfilesdownloaded),len(soundfilesrequired))
-soundfilesneeded = [x for x in soundfilesrequired if x not in soundfilesdownloaded]
+soundfilesneeded = [x for x in soundfilesrequired if x not in combined_sound_files]
 #print([x.replace(".mp3","") for x in soundfilesneeded], len(soundfilesneeded))
 for x in soundfilesneeded:
 	print(x.replace(".mp3",""))
+print(len(soundfilesneeded))
